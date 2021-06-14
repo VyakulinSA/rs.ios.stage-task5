9de5828c090adda7851b80a3caba530c85ea9413
VyakulinSA <VyakulinSA@gmail.com>
2021-06-14 14:56:50 +0400

    compleated 5.4 without 2500 test

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -3,9 +3,9 @@ import Foundation
 public typealias Supply = (weight: Int, value: Int)
 
 public final class Knapsack {
-    let maxWeight: Int
-    let drinks: [Supply]
-    let foods: [Supply]
+    var maxWeight: Int
+    var drinks: [Supply]
+    var foods: [Supply]
     var maxKilometers: Int {
         findMaxKilometres()
     }
@@ -17,6 +17,58 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        
+        let A1 = getA(input: foods)
+        let A2 = getA(input: drinks)
+        
+        var dict1 = [Int:[Supply]]()
+        
+        for i in 0...maxWeight{
+            for j in 0...maxWeight {
+                if A1.last![i] != 0 && A2.last![j] != 0 && i+j<=maxWeight{
+                    dict1[i] = [Supply(i,A1.last![i]),Supply(j,A2.last![j])]
+                }
+            }
+            
+            
+        }
+
+        var minim = 0
+        
+        dict1.forEach { key, value in
+            if minim < min(value[0].value,value[1].value){
+                minim = min(value[0].value,value[1].value)
+            }
+        }
+        
+        return minim
+    }
+    
+    private func getA(input: [Supply])-> [[Int]]{
+        
+        let n = input.count
+        var w = [0]
+        var p = [0]
+
+        input.forEach { weight, value in
+            p.append(value)
+            w.append(weight)
+        }
+        
+        
+        var A = [[Int]](repeating: [Int](repeating: 0, count: maxWeight+1), count: n+1)
+        
+        for k in 1...n{
+            for s in 1...maxWeight{
+                if s >= w[k] {
+                    A[k][s] = max(A[k-1][s], A[k-1][s-w[k]]+p[k])
+                } else {
+                    A[k][s] = A[k-1][s]
+                }
+            }
+        }
+        
+        return A
+        
     }
 }
